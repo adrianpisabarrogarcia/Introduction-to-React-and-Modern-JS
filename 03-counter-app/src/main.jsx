854bf4b3ecd427670919b099cb914741b90b85e2
adrianpisabarrogarcia <adrian.pisabarro.garcia@gmail.com>
2022-09-04 18:03:09 +0200

    Hooks

--- 03-counter-app/src/main.jsx
+++ 03-counter-app/src/main.jsx
@@ -9,16 +9,25 @@ import ThirdApp from './ThirdApp'
 import { CounterApp } from './CounterApp'
 import './styles.css';
 
-ReactDOM.createRoot(document.getElementById('root'))
-.render(
-    <React.StrictMode>
-        <App />
-        <FirstApp />
-        <SecondApp subtitle='Hola, Amaia!!' passNumber={ 1 + 3 }/>
-        <ThirdApp subtitle="Hola, Amaia!!"/>
-        <hr></hr>
-        <CounterApp value={ 2 + 2} />
+// Eventos de React: https://es.reactjs.org/docs/events.html
 
-    </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('root'))
+    .render(
+        <React.StrictMode>
+            <App />
+            <FirstApp />
+            <SecondApp subtitle='Hola, Amaia!!' passNumber={1 + 3} />
+            <ThirdApp subtitle="Hola, Amaia!!" />
+            <hr></hr>
+            <button onClick={(event) => console.log(event)}>
+                Ver objeto evento
+            </button>
+            <button onClick={(event) => console.log(event)}>
+                +1
+            </button>
+            <hr></hr>
+            <CounterApp value={2 + 2} />
+
+        </React.StrictMode>
+    )
 
--- README.md
+++ README.md
@@ -3,10 +3,7 @@ This repository is going to have all the exercises that I'm going to do to learn
 
 ### Create/Start a React project with npm
     $ npx create-react-app nombre-aplicacion
-	$ npm start
-
-### Tun a the project with npm
-    
+	$ npm start    
 
 ### Create/Install dependecies/Run Yarn + Vite + React
 	$ yarn create vite
