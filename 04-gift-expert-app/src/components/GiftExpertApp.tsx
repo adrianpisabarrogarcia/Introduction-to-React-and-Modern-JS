923abe7af7aa867cbc2783fefc06f6764f17d686
Adrián Pisabarro García <adrian.pisabarro.garcia@gmail.com>
2025-04-16 01:17:03 +0200

    Feat: Enhance category input and management in GiftExpertApp
    This commit enriches the `GiftExpertApp` component by improving the category input and management functionality.
    
    - Implemented input validation to prevent adding empty or duplicate categories.
    - Added an alert to notify users about invalid input.
    - Clear the input field after adding a new category.

--- 04-gift-expert-app/src/components/GiftExpertApp.tsx
+++ 04-gift-expert-app/src/components/GiftExpertApp.tsx
@@ -1,5 +1,52 @@
+import { useState } from "react";
+
 export const GiftExpertApp = () => {
+  const [categories, setCategories] = useState([
+    "One Punch",
+    "Samurai X",
+    "Dragon Ball",
+  ]);
+
+  const onAddCategory = () => {
+    if (inputValue.trim().length <= 1) {
+      alert("The input cannot be empty");
+      return;
+    }
+    if (categories.map(cat => cat.toUpperCase()).includes(inputValue.toUpperCase())) {
+      alert("The category already exists");
+      return;
+    }
+    setCategories([...categories, inputValue]);
+    setInputValue("");
+  };
+  
+  const [inputValue, setInputValue] = useState("");
+  
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
   return (
-        <h1>GiftExpertApp</h1>
-  )
-}
+    <>
+      {/* titulo */}
+      <h1>GiftExpertApp</h1>
+
+      {/* Input */}
+      <input
+        id="new-category-input"
+        type="text"
+        placeholder="Buscar Gifs"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <button onClick={onAddCategory}>Agregar</button>
+
+      {/* Listado de Gifs */}
+      <ol>
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ol>
+    </>
+  );
+};
