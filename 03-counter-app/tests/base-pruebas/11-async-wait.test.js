9789a995a297c58740adfa3130acbd137701daeb
Adrián Pisabarro García <adrian.pisabarro.garcia@gmail.com>
2025-04-15 18:03:00 +0200

    Test: Update getImagen tests to handle API key absence
    This commit updates the tests for the `getImagen` function in `11-async-wait.test.js` to specifically test the scenario where the Giphy API key is missing. The test now asserts that the function returns an error message "No se encontró la imagen" when the API key is not available. The previous test that checked for a valid URL is commented out.
    
    - Updated `getImagen` tests to verify error handling when the API key is missing.
    - Commented out the previous test for URL validation.

--- 03-counter-app/tests/base-pruebas/11-async-wait.test.js
+++ 03-counter-app/tests/base-pruebas/11-async-wait.test.js
@@ -5,8 +5,13 @@ jest.setTimeout(10000); // ⬅️ esto permite más tiempo para que termine
 
 
 describe('Pruebas en 11-async-await.js', () => { 
-    test('getImagen debe de retornar un  URL de la imagen', async() => { 
+    // test('getImagen debe de retornar un  URL de la imagen', async() => { 
+    //     const url = await getImagen();
+    //     expect( typeof url ).toBe('string');
+    //  })
+
+     test('getImagen debe de retornar un error si no tenemos api key', async() => { 
         const url = await getImagen();
-        expect( typeof url ).toBe('string');
+        expect( url ).toBe('No se encontró la imagen');
      })
  })
