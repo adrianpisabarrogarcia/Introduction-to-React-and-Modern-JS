ee99cacbe453c56e63d41d79d5a10b2da457d6a3
Adrián Pisabarro García <adrian.pisabarro.garcia@gmail.com>
2025-04-16 00:50:53 +0200

    Refactor: Update GiftExpertApp component and main entry point
    This commit refactors the `GiftExpertApp` component and the main entry point (`main.tsx`) to prepare for further development. It includes the following changes:
    
    - Added comments in `main.tsx` to outline the structure of the application.
    - Imported the global styles in `main.tsx`.
    - Removed unnecessary import statements from `GiftExpertApp.tsx`.

--- 04-gift-expert-app/src/components/GiftExpertApp.tsx
+++ 04-gift-expert-app/src/components/GiftExpertApp.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-import '../index.css'
 export const GiftExpertApp = () => {
   return (
         <h1>GiftExpertApp</h1>
--- 04-gift-expert-app/src/main.tsx
+++ 04-gift-expert-app/src/main.tsx
@@ -1,9 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { GiftExpertApp } from './components/GiftExpertApp'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { GiftExpertApp } from "./components/GiftExpertApp";
 
-createRoot(document.getElementById('root')!).render(
+import "./styles.css";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    {/* titulo */}
     <GiftExpertApp />
-  </StrictMode>,
-)
+    {/* Input */}
+
+    {/* Listado de Gifs */}
+  </StrictMode>
+);
