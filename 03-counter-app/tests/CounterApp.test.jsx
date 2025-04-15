917df31cee321a3f90e1c23040ff16e7e08418ee
Adrián Pisabarro García <adrian.pisabarro.garcia@gmail.com>
2025-04-16 00:29:08 +0200

    Test: Implement CounterApp component tests
    This commit introduces tests for the `CounterApp` component using React Testing Library. The tests cover snapshot rendering, initial value display, increment and decrement button functionality, and the reset button functionality.
    
    - Created `CounterApp.test.jsx` to test the `CounterApp` component.
    - Added snapshot test for `CounterApp`.
    - Added tests to verify initial value display.
    - Added tests to verify increment and decrement button functionality.
    - Added tests to validate reset button functionality.

---
+++ 03-counter-app/tests/CounterApp.test.jsx
@@ -0,0 +1,40 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { CounterApp } from "../src/CounterApp";
+
+
+describe('Pruebas en <CounterApp />', () => {
+
+    const initialValue = 10;
+
+    test('Debe de hacer match con el snapshot', () => {
+        const { container } = render(<CounterApp value={initialValue} />)
+        expect(container).toMatchSnapshot();
+    })
+
+    test('Debe de mostrar el valor inicial sobre 100', () => {
+        render(<CounterApp value={100} />)
+        expect(screen.getByText(100)).toBeTruthy();
+        expect(screen.getByRole('heading', { level: 2 }).innerHTML).toContain('100');
+    })
+
+    test('Debe incrementar con el botón +1', () => {
+        render(<CounterApp value={initialValue} />)
+        fireEvent.click(screen.getByText('+1'));
+        expect(screen.getByText('11')).toBeTruthy();
+    })
+
+    test('Debe decrementar el con el botón -1', () => { 
+        render(<CounterApp value={initialValue} />)
+        fireEvent.click(screen.getByText('-1'));
+        expect(screen.getByText('9')).toBeTruthy();
+     })
+
+    test('Debe de funcionar el botón de reset', () => { 
+        render(<CounterApp value={initialValue} />)
+        fireEvent.click(screen.getByText('+1'));
+        fireEvent.click(screen.getByText('+1'));
+        fireEvent.click(screen.getByText('+1'));
+        fireEvent.click(screen.getByText('Reset'));
+        expect(screen.getByText('10')).toBeTruthy();
+     })
+})
