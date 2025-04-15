54b6ff5cebe9af70bfaf38d264e1e854931d4393
Adrián Pisabarro García <adrian.pisabarro.garcia@gmail.com>
2025-04-15 17:15:30 +0200

    Tests
    Updated tests for 07-deses-arr.test.js to include type and value checks. This enhances test coverage and ensures the returned values are of the expected type and value.

--- 03-counter-app/tests/base-pruebas/07-deses-arr.test.js
+++ 03-counter-app/tests/base-pruebas/07-deses-arr.test.js
@@ -11,5 +11,11 @@ describe('Pruebas en 07-deses-arr', () => {
         expect( letters ).toBe( 'ABC' )
         expect( numbers ).toBe( 123 )
 
+        expect( typeof letters ).toBe( 'string' )
+        expect( typeof numbers ).toBe( 'number' )
+
+        expect( letters ).toEqual( expect.any(String) )
+        expect( numbers ).toEqual( expect.any(Number) )
+
      })
  })
